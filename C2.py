ff3c3ec1d41ac34a20ea40eb9070cb5d0b945dc7
InterNalss <163824769+InterNalsec@users.noreply.github.com>
2024-06-19 13:17:21 +0700

    C2.py

--- C2.py
+++ C2.py
@@ -5,6 +5,34 @@ import random
 import getpass
 import time
 import sys
+import colorama
+
+###color
+def color(data_input_output):
+    color_codes = {
+        "GREEN": '\033[32m',
+        "LIGHTGREEN_EX": '\033[92m',
+        "YELLOW": '\033[33m',
+        "LIGHTYELLOW_EX": '\033[93m',
+        "CYAN": '\033[36m',
+        "LIGHTCYAN_EX": '\033[96m',
+        "BLUE": '\033[34m',
+        "LIGHTBLUE_EX": '\033[94m',
+        "MAGENTA": '\033[35m',
+        "LIGHTMAGENTA_EX": '\033[95m',
+        "RED": '\033[31m',
+        "LIGHTRED_EX": '\033[91m',
+        "BLSYN": '\033[30m',
+        "LIGHTBLSYN_EX": '\033[90m',
+        "WHITE": '\033[37m',
+        "LIGHTWHITE_EX": '\033[97m',
+    }
+
+    return color_codes.get(data_input_output, "")
+
+lightwhite = color("LIGHTWHITE_EX")
+gray = color("LIGHTBLSYN_EX")
+red = color("LIGHTMAGENTA_EX")
 ###include##
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -16,18 +44,18 @@ def home():
     clear()
     si()
     print(f'''
-    \x1b[38;2;0;212;14m██╗   ██╗       ██████╗██████╗ 
-    \x1b[38;2;0;212;14m██║   ██║      ██╔════╝╚════██╗
-    \x1b[38;2;0;212;14m██║   ██║█████╗██║      █████╔╝
-    \x1b[38;2;0;212;14m╚██╗ ██╔╝╚════╝██║     ██╔═══╝ 
-     \x1b[38;2;0;212;14m╚████╔╝       ╚██████╗███████╗
-      \x1b[38;2;0;212;14m╚═══╝         ╚═════╝╚══════╝
-      \x1b[38;2;0;212;14m‎ ╔═════════════════════════╗
-      \x1b[38;2;0;212;14m║ ‎ WELCOME BACK TO V-C2 TOOLS‎‎‎‎ ‎ ‎ ║
-      \x1b[38;2;0;212;14m║‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ CC : V-C2 Team‎ ‎ ‎ ‎ ‎ ‎‎ ‎ ‎ ‎ ║
-      \x1b[38;2;0;212;14m‎║ ‎ ‎ ‎ ‎ ‎TYPE !HELP For help ‎ ‎ ‎ ‎ ‎ ‎ ‎‎║ ‎ ‎ 
-      \x1b[38;2;0;212;14m‎║‎ ‎ ‎ ‎ ‎ ONLINE BOT : {bots}‎ ‎ ‎ ‎ ‎ ‎‎ ‎ ║ ‎ ‎‎
-      \x1b[38;2;0;212;14m ╚═════════════════════════╝
+    {red}██╗   ██╗       ██████╗██████╗ 
+    {red}██║   ██║      ██╔════╝╚════██╗
+    {red}██║   ██║█████╗██║      █████╔╝
+    {red}╚██╗ ██╔╝╚════╝██║     ██╔═══╝ 
+     {red}╚████╔╝       ╚██████╗███████╗
+      {red}╚═══╝         ╚═════╝╚══════╝
+      {lightwhite}‎ ╔═════════════════════════╗
+      {lightwhite}║ ‎ WELCOME BACK TO V-C2 TOOLS‎‎‎‎ ‎ ‎ ║
+      {lightwhite}║‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ CC : V-C2 Team‎ ‎ ‎ ‎ ‎ ‎‎ ‎ ‎ ‎ ║
+      {lightwhite}‎║ ‎ ‎ ‎ ‎ ‎TYPE !HELP For help ‎ ‎ ‎ ‎ ‎ ‎ ‎‎║ ‎ ‎ 
+      {lightwhite}‎║‎ ‎ ‎ ‎ ‎ ONLINE BOT : {bots}‎ ‎ ‎ ‎ ‎ ‎‎ ‎ ║ ‎ ‎‎
+      {lightwhite} ╚═════════════════════════╝
 ''')
 
 def help():
@@ -45,26 +73,25 @@ def help():
 def list():
     si()
     print(f'''
-    \x1b[38;2;0;212;14mLAYER7 ‎ ‎ ‎ = HTTP/HTTPS FLOODER
-    \x1b[38;2;0;212;14mLAYER4  ‎ ‎ = TCP/UDP/SYN FLODDER
-    \x1b[38;2;0;212;14mUAM   ‎ ‎   = BYPASS 
-    \x1b[38;2;0;212;14mEXPLUSIVE = MIRAI/BOTNET
+    {lightwhite}LAYER7 ‎ ‎ ‎ = HTTP/HTTPS FLOODER
+    {lightwhite}LAYER4  ‎ ‎ = TCP/UDP/SYN FLODDER
+    {lightwhite}EXPLUSIVE = MIRAI/BOTNET
     ''')
 
 def L7():
     clear()
     si()
     print(f'''
-\x1b[38;2;0;212;14m██╗      █████╗ ██╗   ██╗███████╗██████╗ ███████╗
-\x1b[38;2;0;212;14m██║     ██╔══██╗╚██╗ ██╔╝██╔════╝██╔══██╗╚════██║
-\x1b[38;2;0;212;14m██║     ███████║ ╚████╔╝ █████╗  ██████╔╝    ██╔╝
-\x1b[38;2;0;212;14m██║     ██╔══██║  ╚██╔╝  ██╔══╝  ██╔══██╗   ██╔╝ 
-\x1b[38;2;0;212;14m███████╗██║  ██║   ██║   ███████╗██║  ██║   ██║  
-\x1b[38;2;0;212;14m╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝                                                   
-\x1b[38;2;0;212;14m╔═══════════════════════════════════════════════╗
-\x1b[38;2;0;212;14m║‎ ‎‎‎ HTTP-ZEUZ‎   ‎║‎ HTTP-BROWSER ║ ‎ ‎ HTTP-TLS ‎ ‎‎ ║ HTTP-RAND║
-\x1b[38;2;0;212;14m║ HTTP-ENGINE ‎ ║ ‎ HTTP-STROM ‎ ║‎  HTTP-SOCKET‎ ║‎ HTTP-HCAP║
-\x1b[38;2;0;212;14m╚═══════════════════════════════════════════════╝
+    \x1b[38;2;0;212;14m██╗      █████╗ ██╗   ██╗███████╗██████╗ ███████╗
+    \x1b[38;2;0;212;14m██║     ██╔══██╗╚██╗ ██╔╝██╔════╝██╔══██╗╚════██║
+    \x1b[38;2;0;212;14m██║     ███████║ ╚████╔╝ █████╗  ██████╔╝    ██╔╝
+    \x1b[38;2;0;212;14m██║     ██╔══██║  ╚██╔╝  ██╔══╝  ██╔══██╗   ██╔╝ 
+    \x1b[38;2;0;212;14m███████╗██║  ██║   ██║   ███████╗██║  ██║   ██║  
+    \x1b[38;2;0;212;14m╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝                                                   
+    \x1b[38;2;0;212;14m╔═══════════════════════════════════════════════╗
+    \x1b[38;2;0;212;14m║‎ ‎‎‎ HTTP-ZEUZ‎   ‎║‎ HTTP-BROWSER ║ ‎ ‎ HTTP-TLS ‎ ‎‎ ║ HTTP-RAND║
+    \x1b[38;2;0;212;14m║ HTTP-ENGINE ‎ ║ ‎ HTTP-STROM ‎ ║‎  HTTP-SOCKET‎ ║‎ HTTP-HCAP║
+    \x1b[38;2;0;212;14m╚═══════════════════════════════════════════════╝
 ''')
 
 
